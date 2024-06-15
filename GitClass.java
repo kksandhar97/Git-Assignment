5c3efee808bac1f0eecec9792cf57343d3664215
kksandhar97 <kksandhar97@gmail.com>
2024-06-15 00:23:25 -0400

    Scanner added

--- GitClass.java
+++ GitClass.java
@@ -1,12 +1,11 @@
+import java.util.Scanner;
+
 public class GitClass {
     public static void main(String[] args) {
         System.out.println("Hello java");
         System.out.println("Hello java");
         System.out.println("Hello java");
-        System.out.println("Hello java");
-        System.out.println("Hello java");
-        System.out.println("Hello java");
-
+        Scanner sc = new Scanner (System.in);
 
 
 
