4144f3bcf4379ddbf9fe75d289d87f96f311125b
kksandhar97 <kksandhar97@gmail.com>
2024-06-15 00:11:59 -0400

    June 13

---
+++ June13.java
@@ -0,0 +1,8 @@
+import javax.sound.midi.Soundbank;
+
+public class June13 {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+        System.out.println("Enter your input");
+    }
+}
